d9fefab0e025ff3e4c21664cbc7ce35f9afad318
IAmTerror <cyrilberthout@gmail.com>
2020-01-26 21:14:53 +0100

    trello sample get_card_by_id() done

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
+#            )                              (                                                   )
+#         ( /(         (        (      )    )\ )      (  (                    (   (          ( /(
+#  `  )   )\())  (    ))\  (    )\  ( /(   (()/(  (   )\))(    (      (    (  )(  )\  `  )   )\())
+#  /(/(  ((_)\   )\  /((_) )\ )((_) )\())   ((_)) )\ ((_)()\   )\ )   )\   )\(()\((_) /(/(  (_))/
+# ((_)_\ | |(_) ((_)(_))  _(_/( (_)((_)\    _| | ((_)_(()((_) _(_/(  ((_) ((_)((_)(_)((_)_\ | |_
+# | '_ \)| ' \ / _ \/ -_)| ' \))| |\ \ /  / _` |/ _ \\ V  V /| ' \)) (_-</ _|| '_|| || '_ \)|  _|
+# | .__/ |_||_|\___/\___||_||_| |_|/_\_\  \__,_|\___/ \_/\_/ |_||_|  /__/\__||_|  |_|| .__/  \__|
+# |_|
+
+
+# Author :
+# +-+-+-+-+-+-+-+-+-+
+# |I|A|m|T|e|r|r|o|r|
+# +-+-+-+-+-+-+-+-+-+
+
+# Licence :
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+# Notes :
+# Python script tested on Ubuntu Linux. It can run on Windows with minor adjustements.
+
+
+########################################################################################################################
+
+########################################################################################################################
+
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from trello_sample import *
+from constants import *
 
 
-### SAMPLE REQUESTS ###
-# get_board_by_id()
-# get_open_cards_by_board_id()
+# SAMPLE REQUESTS ------------------------------------------------------------------------------------------------------
+# get_board_by_id(TRELLO_MBL_BOARD_ID)
+# get_open_cards_by_board_id(TRELLO_MBL_BOARD_ID)
+get_card_by_id(TRELLO_CH_CARD_ID)
