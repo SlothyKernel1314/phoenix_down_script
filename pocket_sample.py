50b5bfb3c7ec337956e642c36d186dc738873205
IAmTerror <cyrilberthout@gmail.com>
2020-08-11 23:34:27 +0200

    pocket_sample.py Oauth 2.0 process clean

--- pocket_sample.py
+++ pocket_sample.py
@@ -2,21 +2,12 @@
 # !/usr/bin/env python
 
 from credentials import *
-from constants import *
 import requests
 import requests.auth
 import os
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium import webdriver
 import sys
-
-
-# For Firefox we need to set a headless flag to inform browser not to use gui.
-firefox_bin = '/usr/bin/geckodriver'
-os.environ['MOZ_HEADLESS'] = '1'
-redirect_uri = 'pocketapp1234:authorizationFinished'
-user = POCKET_USERNAME
-password = POCKET_PASSWORD
 
 
 def pocket_request_token():
@@ -34,14 +25,18 @@ def authorize_app_and_get_access_token():
     code = pocket_request_token()
     redirect_uri = "pocketapp1234:authorizationFinished"
 
+    # For Firefox we need to set a headless flag to inform browser not to use gui.
+    firefox_bin = '/usr/bin/geckodriver'
+    os.environ['MOZ_HEADLESS'] = '1'
+
     # We are using Selenium to send login form with obtained in previous step request token. Keep in mind that different
     # providers will have different login form. You need to check field ids you will be populating with data.
     binary = FirefoxBinary(firefox_bin, log_file=sys.stdout)
 
     driver = webdriver.Firefox()
     driver.get('https://getpocket.com/auth/authorize?request_token={}&redirect_uri={}'.format(code, redirect_uri))
-    driver.find_element_by_id('feed_id').send_keys(user)
-    driver.find_element_by_id('login_password').send_keys(password)
+    driver.find_element_by_id('feed_id').send_keys(POCKET_USERNAME)
+    driver.find_element_by_id('login_password').send_keys(POCKET_PASSWORD)
     driver.find_element_by_class_name('btn-authorize').click()
     driver.close()
 
@@ -51,9 +46,7 @@ def authorize_app_and_get_access_token():
     response = requests.request("POST", url, params=payload)
     datas = response.text
     split_datas = datas.replace('=', ' ').replace('&', ' ').split()
-    print(split_datas)
     access_token = split_datas[1]
-    print(access_token)
     return access_token
 
 
