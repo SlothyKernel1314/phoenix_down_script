d866e76afab74bc0672b18862b86772605de6a20
IAmTerror <cyrilberthout@gmail.com>
2020-08-20 15:45:40 +0200

    youtube_script.py wip

--- youtube_script.py
+++ youtube_script.py
@@ -51,6 +51,44 @@ class YoutubeScript:
         else:
             return all_datas, nb_suscriptions
 
+    def get_playlists_by_channel_id_with_exceptions(self, channel_id):
+        youtube = self.get_authenticated_service()
+        request = youtube.channels().list(
+            part="snippet,contentDetails,statistics",
+            id=channel_id
+        )
+        response = request.execute()
+        items = response['items'][0]
+        playlists = items['contentDetails']['relatedPlaylists']
+        # remove useless playlists
+        playlists.pop('likes')
+        playlists.pop('uploads')
+        playlists.pop('watchHistory')
+        playlists.pop('watchLater')
+        # add a playlist that is not detected by the method get_playlists_by_channel_id_with_exceptions()
+        playlists["later"] = YOUTUBE_USER_PLAYLIST_LATER_ID
+        return playlists
+
+    def get_playlist_items_by_playlist_id(self, playlist_id, next_page_token=None, all_datas=None):
+        if all_datas is None:
+            all_datas = []
+        youtube = self.get_authenticated_service()
+        request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            maxResults=50,
+            playlistId=playlist_id,
+            pageToken=None if next_page_token is None else next_page_token
+        )
+        response = request.execute()
+        all_datas.append(response)
+        if 'nextPageToken' in response:
+            next_page_token = response['nextPageToken']
+        nb_suscriptions = response['pageInfo']['totalResults']
+        if 'nextPageToken' in response and next_page_token is not None:
+            return self.get_playlist_items_by_playlist_id(playlist_id, next_page_token, all_datas)
+        else:
+            return all_datas, nb_suscriptions
+
     def run_script(self):
         logging.info('youtube script is running...')
 
@@ -59,6 +97,8 @@ class YoutubeScript:
         my_subscriptions = self.get_my_subscriptions()
         my_subscriptions_all_datas = my_subscriptions[0]
         my_subscriptions_count = my_subscriptions[1]
+
+        my_playlists = self.get_playlists_by_channel_id_with_exceptions(YOUTUBE_USER_ID)
 
         logging.info('creating youtube log file')
         file_name = create_timestamped_and_named_file_name(self.application_name)
@@ -83,6 +123,8 @@ class YoutubeScript:
             file.write(str(json))
             file.write("\n\n\n\n")
         file.write("\n\n\n\n")
+
+
 
 
 
