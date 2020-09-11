4f63d82bd159c4d60227071dc5d489d10464a7f9
IAmTerror <cyrilberthout@gmail.com>
2020-09-11 16:30:23 +0200

    reddit_script.py exceptions

--- reddit_script.py
+++ reddit_script.py
@@ -17,7 +17,7 @@ class RedditScript:
         post_data = {"grant_type": "password", "username": REDDIT_USERNAME, "password": REDDIT_PASSWORD}
         headers = {"User-Agent": "phoenix-down/0.1 by IAmTerror"}
         response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             auth=client_auth, data=post_data, headers=headers)
+                                 auth=client_auth, data=post_data, headers=headers)
         try:
             response.raise_for_status()
             datas = response.json()
@@ -31,11 +31,16 @@ class RedditScript:
         url = "https://oauth.reddit.com/api/v1/me"
         headers = {"Authorization": "bearer " + token, "User-Agent": "phoenix-down/0.1 by IAmTerror"}
         response = requests.get(url, headers=headers)
-        datas = response.json()
-        username = datas['name']
+        try:
+            response.raise_for_status()
+            datas = response.json()
+            username = datas['name']
+        except requests.exceptions.HTTPError as e:
+            logging.warning("Error: " + str(e))
+            username = ""
         return username
 
-    def get_saved_posts(self, token, after_pagination=None, saved_posts_count=0,  all_datas=None):
+    def get_saved_posts(self, token, after_pagination=None, saved_posts_count=0, all_datas=None):
         if all_datas is None:
             all_datas = []
         if after_pagination is None:
@@ -81,54 +86,52 @@ class RedditScript:
 
         token = self.reddit_request_token()
 
-        username = self.get_username(token)
+        if len(token) > 0:
 
-        saved_posts = self.get_saved_posts(token)
+            username = self.get_username(token)
 
-        suscribed_subreddits = self.get_subscribed_subreddits(token)
+            saved_posts = self.get_saved_posts(token)
 
-        logging.info('creating reddit log file')
-        file_name = create_timestamped_and_named_file_name(self.application_name)
-        file = open(file_name, "w", encoding="utf-8")
+            suscribed_subreddits = self.get_subscribed_subreddits(token)
 
-        logging.info('writing in reddit log file...')
-        # processing of saved posts
-        file.write("##### Saved posts of " + username + " reddit user (JSON) :")
-        file.write("\n\n")
-        file.write(username + " reddit user have " + str(saved_posts[1]) + " saved posts")
-        file.write("\n\n")
-        for json in saved_posts[0]:
-            file.write(str(json))
-            file.write("\n\n\n\n")
-        # processing of subreddits
-        file.write("##### Suscribed subreddits of " + username + " reddit user (list) :")
-        file.write("\n\n")
-        file.write(username + " reddit user have " + str(suscribed_subreddits[1]) + " suscribed subreddits")
-        file.write("\n\n")
-        for json in suscribed_subreddits[0]:
-            children = json['data']['children']
-            for subreddit in children:
-                file.write(subreddit['data']['display_name_prefixed'])
-                file.write("\n")
-        file.write("\n\n")
-        file.write("##### Suscribed subreddits of " + username + " reddit user (JSON) :")
-        file.write("\n\n")
-        for json in suscribed_subreddits[0]:
-            file.write(str(json))
-            file.write("\n\n\n\n")
+            logging.info('creating reddit log file')
+            file_name = create_timestamped_and_named_file_name(self.application_name)
+            file = open(file_name, "w", encoding="utf-8")
 
-        logging.info('writing in reddit log file done')
-        file.close()
+            logging.info('writing in reddit log file...')
+            # processing of saved posts
+            file.write("##### Saved posts of " + username + " reddit user (JSON) :")
+            file.write("\n\n")
+            file.write(username + " reddit user have " + str(saved_posts[1]) + " saved posts")
+            file.write("\n\n")
+            for json in saved_posts[0]:
+                file.write(str(json))
+                file.write("\n\n\n\n")
+            # processing of subreddits
+            file.write("##### Suscribed subreddits of " + username + " reddit user (list) :")
+            file.write("\n\n")
+            file.write(username + " reddit user have " + str(suscribed_subreddits[1]) + " suscribed subreddits")
+            file.write("\n\n")
+            for json in suscribed_subreddits[0]:
+                children = json['data']['children']
+                for subreddit in children:
+                    file.write(subreddit['data']['display_name_prefixed'])
+                    file.write("\n")
+            file.write("\n\n")
+            file.write("##### Suscribed subreddits of " + username + " reddit user (JSON) :")
+            file.write("\n\n")
+            for json in suscribed_subreddits[0]:
+                file.write(str(json))
+                file.write("\n\n\n\n")
 
-        # opens the file for reading only in binary format in order to upload
-        file = open(file_name, "rb")
+            logging.info('writing in reddit log file done')
+            file.close()
 
-        upload_file_to_server_ftp(file, file_name, self.application_name)
+            # opens the file for reading only in binary format in order to upload
+            file = open(file_name, "rb")
 
-        file.close()
+            upload_file_to_server_ftp(file, file_name, self.application_name)
+
+            file.close()
 
         logging.info('reddit script is terminated')
-
-
-
-
